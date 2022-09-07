d9ac9d775d48b6ca8fd4d903f3e76ea275f4bbac
intking <dwi.wahyu.toso@gmail.com>
2022-09-07 20:50:51 +0700

    add escrow contract

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -8,16 +8,83 @@ interface IERC721 {
 contract Escrow {
   address public nftAddress;
   uint256 public nftID;
+  uint256 public purchasePrice;
+  uint256 public escrowAmount;
   address payable seller;
   address payable buyer;
+  address public lender;
+  address public inspector;
 
-  constructor(address _nftAddress, uint256 _nftID, address payable _seller, address payable _buyer) {
+  modifier onlyBuyer() {
+    require(msg.sender == buyer, "Only buyer can call this function");
+    _; 
+  }
+
+  modifier onlyInspector() {
+    require(msg.sender == inspector, "Only inspector can call this function");
+    _; 
+  }
+
+  receive() external payable {}
+
+  bool inspectionPassed = false;
+  mapping( address => bool ) public approval;
+
+  constructor(
+    address _nftAddress,
+    uint256 _nftID,
+    uint256 _purchasePrice, 
+    uint256 _escrowAmount, 
+    address payable _seller, 
+    address payable _buyer, 
+    address _inspector, 
+    address _lender) {
     nftAddress = _nftAddress;
     nftID = _nftID;
+    purchasePrice = _purchasePrice;
+    escrowAmount = _escrowAmount;
     seller = _seller;
     buyer = _buyer;
+    inspector = _inspector;
+    lender = _lender;
   }
+
+  function depositEarnest() public payable onlyBuyer {
+    require(msg.value >= escrowAmount);
+
+  }
+
+  function updateInspectionStatus(bool _passed) public onlyInspector {
+    inspectionPassed = _passed;
+
+  }
+
+  function approveSale() public {
+    approval[msg.sender] = true;
+  }
+
+  function getBalance() public view returns (uint) {
+    return address(this).balance;
+  }
+
+  function cancelSale() public {
+    if(inspectionPassed == false) {
+      payable(buyer).transfer(address(this).balance);
+    } else {
+      payable(seller).transfer(address(this).balance);
+    }
+  }
+
   function finalizeSale() public {
+    require(inspectionPassed, "Must pass inspection");
+    require(approval[buyer], "Must be approved by buyer");
+    require(approval[seller], "Must be approved by seller");
+    require(approval[lender], "Must be approved by lender");
+    require(address(this).balance >= purchasePrice, "Must have enough ether for sale");
+
+    (bool success, ) = payable(seller).call{value: address(this).balance}("");
+    require(success);
+
     IERC721(nftAddress).transferFrom(seller, buyer, nftID);
   }
 }
--- test/RealEstate.js
+++ test/RealEstate.js
@@ -1,16 +1,25 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
+
+const ether = tokens;
 describe("RealEstate", () => {
   let realEstate, escrow, transaction;
-  let accounts, deployer, seller, buyer;
+  let accounts, deployer, seller, buyer, inspector, lender;
   let nftID = 1;
+  let purchasePrice = ether(100);
+  let escrowAmount = ether(20);
 
   beforeEach(async () => {
     accounts = await ethers.getSigners();
     deployer = accounts[0];
     seller = deployer;
     buyer = accounts[1];
+    inspector = accounts[2];
+    lender = accounts[3];
 
     const RealEstate = await ethers.getContractFactory("RealEstate");
     const Escrow = await ethers.getContractFactory("Escrow");
@@ -19,8 +28,12 @@ describe("RealEstate", () => {
     escrow = await Escrow.deploy(
       realEstate.address,
       nftID,
+      purchasePrice,
+      escrowAmount,
       seller.address,
-      buyer.address
+      buyer.address,
+      inspector.address,
+      lender.address
     );
 
     transaction = await realEstate
@@ -36,16 +49,51 @@ describe("RealEstate", () => {
   });
 
   describe("Selling real estate", async () => {
+    let transaction, balance;
     it("executes a successful transaction", async () => {
       expect(await realEstate.ownerOf(nftID)).to.equal(seller.address);
+
+      balance = await escrow.getBalance();
+      console.log("Escrow balance: ", ethers.utils.formatEther(balance));
+
+      transaction = await escrow
+        .connect(buyer)
+        .depositEarnest({ value: escrowAmount });
+      console.log("Buyer deposit earnest money");
+
+      balance = await escrow.getBalance();
+      console.log("Escrow balance: ", ethers.utils.formatEther(balance));
+
+      transaction = await escrow
+        .connect(inspector)
+        .updateInspectionStatus(true);
+      console.log("Inspector updates status");
+
+      transaction = await escrow.connect(buyer).approveSale();
+      console.log("Buyer approves sale");
+
+      transaction = await escrow.connect(seller).approveSale();
+      console.log("Seller approves sale");
+
+      transaction = await lender.sendTransaction({
+        to: escrow.address,
+        value: ether(80),
+      });
+
+      transaction = await escrow.connect(lender).approveSale();
+      console.log("Lender approves sale");
 
       transaction = await escrow.connect(buyer).finalizeSale();
       await transaction.wait();
       console.log("Buyer finalizes sale");
 
       expect(await realEstate.ownerOf(nftID)).to.equal(buyer.address);
+
+      balance = await ethers.provider.getBalance(seller.address);
+      console.log("Seller balance:", ethers.utils.formatEther(balance));
+      expect(balance).to.be.above(ether(10099));
     });
   });
 });
 
-//min 1.35
+//min 2.09
